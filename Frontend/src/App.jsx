96ecf608d34592308cdbfb40b02b0708f12c54fb
krishna26605 <krishnaanikam26@gmail.com>
2025-07-08 23:01:47 +0530

    user UI completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,6 +12,7 @@ import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 import { CaptainDataContext } from './context/CaptainContext'
+import Riding from './pages/Riding'
 
 
 const App = () => {
@@ -22,6 +23,8 @@ const App = () => {
       <Routes>
         <Route path = '/' element= {<Start />} />
         <Route path = '/login' element= {<UserLogin />} />
+        <Route path = '/riding' element= {<Riding />} />
+
         <Route path = '/signup' element= {<UserSignup />} />
         <Route path = '/captain-login' element= {<CaptainLogin />} />
         <Route path = '/captain-signup' element= {<CaptainSignup />} />
--- Frontend/src/pages/Riding.jsx
+++ Frontend/src/pages/Riding.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom' 
 
-const WaitingForDriver = (props) => {
+
+const Riding = () => {
   return (
-    <div>
-        <h5 className='p-1 text-center w-[93%] absolute  top-0 ' onClick={()=>{
-            props.waitingForDriver(false)
-          }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
+    <div className='h-screen'>
+
+        <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+            <i className="text-lg font-medium ri-home-4-line"></i>
+        </Link>
+        <div className='h-1/2'>
+        <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+            
+        </div>
+
+        <div className='h-1/2 p-4'>
             <div className= 'flex items-center justify-between'>
                 <img className='h-12' src="https://i.pinimg.com/736x/a7/44/bb/a744bb6640c985cf72395ae7c61f3eed.jpg" alt="" />
                 <div className='text-right'>
@@ -19,13 +28,7 @@ const WaitingForDriver = (props) => {
             <div className='flex justify-between gap-2 flex-col items-center'>
             
                     <div className='w-full mt-5'>
-                        <div className='flex items-center gap-5 p-3 border-b-2'>
-                            <i className="ri-map-pin-user-fill"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>562/11-A</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Dwarka Circle , Nashik</p>
-                            </div>
-                        </div>
+                        
                         <div className='flex items-center gap-5 p-3 border-b-2'>
                             <i className="text-lg ri-map-pin-fill"></i>
                             <div>
@@ -43,9 +46,11 @@ const WaitingForDriver = (props) => {
 
                     </div>
             </div>
-
+            <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Make a Payment</button>
+        </div>
+        
     </div>
   )
 }
 
-export default WaitingForDriver
+export default Riding
