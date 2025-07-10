b67ef3894bb876e3622223498ba7c260d11f212a
krishna26605 <krishnaanikam26@gmail.com>
2025-07-10 20:53:51 +0530

    google api configuration

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const connectToDatabase = require('./db/db.js');
 const usersRoutes = require('./routes/user.routes.js');
 const captainRoutes = require('./routes/captain.routes.js');
+const mapsRoutes = require('./routes/maps.routes.js')
 
 connectToDatabase();
 app.use(cors());
@@ -23,4 +24,5 @@ app.get('/', (req, res) => {
 
 app.use('/users', usersRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes)
 module.exports = app;
